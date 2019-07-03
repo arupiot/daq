223cc3fdb36bc651b3025be604f9ad91135a91e5
Jason Brewer <jason@urawizard.com>
2019-07-03 18:01:15 +0100

    Travis debugging

--- daq/gcp.py
+++ daq/gcp.py
@@ -42,6 +42,8 @@ class GcpManager:
         LOGGER.info('Loading gcp credentials from %s', cred_file)
         # Normal execution assumes default credentials.
         # pylint: disable=protected-access
+        f = open(cred_file, 'r')
+        print(f.read())
         (self._credentials, self._project) = google_auth._load_credentials_from_file(cred_file)
         self._client_name = self._parse_creds(cred_file)
         self._pubber = pubsub_v1.PublisherClient(credentials=self._credentials)
