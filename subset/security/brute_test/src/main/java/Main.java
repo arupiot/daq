f5477ff8d3e82e7b2b120575272be7cdcf8de049
Ketan Tak <ketan.tak@redstone.com>
2019-09-02 08:40:39 +0100

    commit before sync of fork with upstream on arupiot

--- subset/security/brute_test/src/main/java/Main.java
+++ subset/security/brute_test/src/main/java/Main.java
@@ -2,10 +2,8 @@ public class Main {
 
   public static void main(String[] args) {
 
-    // telnet OR ssh AS A PROTOCOL
-
     if (args.length != 3) {
-      throw new IllegalArgumentException("Usage: target_ip protocol(ssh) target_port target_mac");
+      throw new IllegalArgumentException("Usage: target_ip target_port target_mac");
     }
 
     String host = args[0];
